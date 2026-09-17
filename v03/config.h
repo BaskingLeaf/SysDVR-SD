fcdee90efe8f15b8ee3cac314f4448c583b514e5
BaskingLeaf <david.prestt@gmail.com>
2026-09-17 14:41:35 +0100

    Add v0.3 buffered recorder config

---
+++ v03/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define SYSDVR_SD_VERSION "0.3.0-buffered"
+#define RECORD_DIR "sdmc:/switch/SysDVR-SD"
+#define CONTROL_FILE RECORD_DIR "/recording.enabled"
+#define ACTIVE_FILE RECORD_DIR "/recording.active"
+#define SEGMENT_MINUTES 30u
+#define MIN_FREE_MIB 512u
+#define FLUSH_EVERY_BYTES (2u * 1024u * 1024u)
+
+#define VIDEO_BUFFER_SIZE 0x54000u
+#define AUDIO_CHUNK_SIZE 0x1000u
+#define AUDIO_BATCH_CHUNKS 4u
+#define AUDIO_BUFFER_SIZE (AUDIO_CHUNK_SIZE * AUDIO_BATCH_CHUNKS)
+
+#define VIDEO_QUEUE_SLOTS 8u
+#define AUDIO_QUEUE_SLOTS 64u
+
+#define VIDEO_WIDTH 1280u
+#define VIDEO_HEIGHT 720u
+#define VIDEO_FPS 30u
+#define AUDIO_SAMPLE_RATE 48000u
+#define AUDIO_CHANNELS 2u
+#define AUDIO_BITS_PER_SAMPLE 16u
